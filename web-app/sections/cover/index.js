fa6defd816edeff6281c75f2ee2764dbc32776c6
Dave Sag <davesag@gmail.com>
2018-03-22 22:23:18 +1030

    DEV-200 fixed linting and linting config

--- web-app/sections/cover/index.js
+++ web-app/sections/cover/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 
-import s from './index.scss';
+import s from './index.scss'
 
-console.log(s);
+console.log(s)
 
-export default function AboutPage ({...rest}) {
-  return (
-    <section className={s['container']}>
-    </section>
-  )
+export default function AboutPage({ ...rest }) {
+  return <section className={s['container']} />
 }
