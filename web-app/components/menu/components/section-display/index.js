b71b2a3eeee4c1a9e1c3971d4f24f6da36783529
smoliver <samuelolive21@gmail.com>
2018-03-28 22:11:55 -0400

    #DEV-186 WIP Added animation container

---
+++ web-app/components/menu/components/section-display/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import sVariables from '!sass-variable-loader!!../../_variables.scss'
+
+const { activeMenuWidth, latentMenuWidth } = sVariables
+
+const activeWidth = parseInt(activeMenuWidth),
+  latentWidth = parseInt(latentMenuWidth)
+
+export default function SectionDisplay({
+  children,
+  open,
+  style = {},
+  ...rest
+}) {
+  const displayStyle = {
+    ...style,
+    width: open * (activeWidth - latentWidth) + latentWidth
+  }
+
+  return (
+    <div style={displayStyle} {...rest}>
+      {children}
+    </div>
+  )
+}
