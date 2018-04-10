29ef0a13ba979716ecd6a004a9478ad4c5d2d8f5
smoliver <samuelolive21@gmail.com>
2018-04-10 14:27:58 -0400

    DEV-90 webpack.config.test updated to work with code coverage

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,6 +1,31 @@
-var isCoverage = process.env.NODE_ENV === 'coverage'
+const isCoverage = process.env.NODE_ENV === 'coverage'
+var nodeExternals = require('webpack-node-externals')
+const path = require('path')
 const config = require('./webpack.config.js')
 
-config.devtool = 'cheap-module-eval-source-map'
-
-module.exports = config
+module.exports = (env, argv = { mode: 'development' }) => {
+  const baseConfig = config(env, argv)
+  return {
+    ...baseConfig,
+    output: {
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+      devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+    },
+    devtool: 'cheap-module-eval-source-map',
+    module: {
+      rules: [
+        isCoverage
+          ? {
+              test: /\.js/,
+              include: path.resolve('web-app'),
+              loader: 'istanbul-instrumenter-loader'
+            }
+          : [],
+        ...baseConfig.module.rules
+      ]
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    devtool: 'inline-cheap-module-source-map'
+  }
+}
