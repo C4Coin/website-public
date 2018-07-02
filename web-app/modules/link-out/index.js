0e909ff5cd07a987309a13981e8e83f3827abf97
smoliver <samuelolive21@gmail.com>
2018-07-02 17:28:24 -0400

    DEV-420 Linted

--- web-app/modules/link-out/index.js
+++ web-app/modules/link-out/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import domUtils from 'utils/dom-utils'
+
+LinkOut.propTypes = {
+  to: PropTypes.string.isRequired
+}
 
 export default function LinkOut({ to, ...rest }) {
   return domUtils.isInternalUrl(to) ? (
