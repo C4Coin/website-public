afb4c5b645eb17a86064d1e81a06fd6125788fd8
smoliver <samuelolive21@gmail.com>
2018-07-17 16:25:39 -0400

    DEV-432 re-styled.  Could still use animations

--- web-app/sections/technology/index.js
+++ web-app/sections/technology/index.js
@@ -4,7 +4,7 @@ import OpenSourcePromise from './sections/open-source-promise'
 import Papers from './sections/papers'
 import Timeline from './sections/timeline'
 import appConfig from 'app.config.js'
-import s from './index.scss'
+import s from './style/index.scss'
 
 const whitePaper =
   'https://github.com/C4Coin/whitepaper/blob/master/c4coin-wp-v11.pdf'
@@ -18,8 +18,10 @@ export default function Technology({ ...rest }) {
   return (
     <Page>
       <div className={s['technology-section']}>
-        <h2 className={s['technology-title']}>Technology</h2>
-        <OpenSourcePromise github={github} />
+        <div className={s['above-the-fold']}>
+          <h2 className={s['technology-title']}>Technology</h2>
+          <OpenSourcePromise github={github} />
+        </div>
       </div>
       <Papers whitePaperUrl={whitePaper} litePaperUrl={litePaper} />
       <Timeline />
