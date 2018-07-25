5a3d288da192758b12c62205ae261aa82c1eac58
smoliver <samuelolive21@gmail.com>
2018-07-25 19:06:22 -0400

    1.0.1 Updated Description

--- base/base.config.js
+++ base/base.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   title: 'C4Coin',
-  description:
-    'A Blockchain rewarding everyone for protecting the protecting the environment',
+  description: 'A Blockchain rewarding everyone for protecting the environment',
   url: 'http://c4coin.org',
   containerId: 'app-container'
 }
