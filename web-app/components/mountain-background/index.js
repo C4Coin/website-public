2c5d3a86e97453f98e47168acadf60aa497800c7
smoliver <samuelolive21@gmail.com>
2018-08-31 03:01:00 -0400

    DEV-453 timer styled

---
+++ web-app/components/mountain-background/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import mountains from 'assets/graphics/token-genesis-mountain.png'
+import s from './index.scss'
+
+export default function Banner({ text = 'CO2KN', className = '', ...rest }) {
+  return (
+    <div className={`${className} ${s['container']}`} {...rest}>
+      <div className={s['sky']} {...rest}>
+        <p className={s['text']}>{text}</p>
+        <img src={mountains} className={s['mountains']} />
+        <div className={s['transition']} />
+      </div>
+    </div>
+  )
+}
