700f7c220abeacfa2a60de910954e29697089b5c
smoliver <samuelolive21@gmail.com>
2018-10-22 19:46:54 -0400

    DEV-488 added prop-types to tracked clickables

--- web-app/modules/analytics/with-onclick-tracking.js
+++ web-app/modules/analytics/with-onclick-tracking.js
@@ -4,11 +4,17 @@ import PropTypes from 'prop-types'
 import ReactGA from 'react-ga'
 
 export default function withOnclickTracking(Component, event) {
-  return function TrackedClickComponent({ onClick, ...rest }) {
+  const ComponentWithTrackedClick = ({ onClick = () => {}, ...rest }) => {
     const trackedClick = () => {
       ReactGA.event(event)
       onClick()
     }
     return <Component onClick={trackedClick} {...rest} />
   }
+
+  ComponentWithTrackedClick.propTypes = {
+    onClick: PropTypes.function
+  }
+
+  return ComponentWithTrackedClick
 }
