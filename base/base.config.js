8031a8cb6bc01f634d63a9a8062e21e9de88de99
smoliver <samuelolive21@gmail.com>
2018-10-31 12:49:55 -0400

    updated description language

--- base/base.config.js
+++ base/base.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'C4Coin',
-  description: 'A Blockchain rewarding everyone for protecting the environment',
+  description:
+    'The C4Coin platform fights climage change by rewarding everyone for protecting the environment',
   url: 'http://c4coin.org',
   containerId: 'app-container',
   facebookPixel: process.env.FACEBOOK_PIXEL
